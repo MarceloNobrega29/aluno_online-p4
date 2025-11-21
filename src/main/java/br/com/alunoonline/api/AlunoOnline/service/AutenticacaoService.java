51aec2a70f49b8e51e438849a31b8749f68813ec
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-11-21 05:40:15 -0300

    feat: finalizando service de autenticacao

--- src/main/java/br/com/alunoonline/api/AlunoOnline/service/AutenticacaoService.java
+++ src/main/java/br/com/alunoonline/api/AlunoOnline/service/AutenticacaoService.java
@@ -1,4 +1,20 @@
 package br.com.alunoonline.api.AlunoOnline.service;
 
-public class AutenticacaoService {
+import br.com.alunoonline.api.AlunoOnline.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
 }
