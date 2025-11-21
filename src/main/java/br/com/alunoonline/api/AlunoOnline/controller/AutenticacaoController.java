3ee6d253b269728635a727d3bf9aa5c27ae4deb0
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-11-21 10:18:45 -0300

    refactor: Separando login e cadastro em controllers distintos

--- src/main/java/br/com/alunoonline/api/AlunoOnline/controller/AutenticacaoController.java
+++ src/main/java/br/com/alunoonline/api/AlunoOnline/controller/AutenticacaoController.java
@@ -13,9 +13,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/login")
 public class AutenticacaoController {
 
     @Autowired
@@ -24,13 +26,7 @@ public class AutenticacaoController {
     @Autowired
     private TokenService tokenService;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/login")
+    @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(authenticationToken);
@@ -38,21 +34,6 @@ public class AutenticacaoController {
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
-    }
-
-    @PostMapping("/cadastros")
-    public ResponseEntity cadastrar(@RequestBody @Valid DadosAutenticacao dados) {
-        if (usuarioRepository.findByLogin(dados.login()) != null) {
-            return ResponseEntity.badRequest().body("Usuário já existe");
-        }
-
-        Usuario usuario = new Usuario();
-        usuario.setLogin(dados.login());
-        usuario.setSenha(passwordEncoder.encode(dados.senha()));
-
-        usuarioRepository.save(usuario);
-
-        return ResponseEntity.ok("Usuário cadastrado com sucesso");
     }
 
     private record DadosTokenJWT(String token) {}
