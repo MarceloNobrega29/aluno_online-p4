a4fc450a2c6a1f952f19754e90715558b13a93e8
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-11-21 10:27:56 -0300

    feat: Adicionando DTOs de cadastro de usuario

--- src/main/java/br/com/alunoonline/api/AlunoOnline/dtos/DadosAutenticacao.java
+++ src/main/java/br/com/alunoonline/api/AlunoOnline/dtos/DadosAutenticacao.java
@@ -1,6 +1,11 @@
 package br.com.alunoonline.api.AlunoOnline.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DadosAutenticacao(
+        @NotBlank
         String login,
+
+        @NotBlank
         String senha
 ) {}
